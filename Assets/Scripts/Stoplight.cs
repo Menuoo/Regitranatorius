a23d38fc7084b1486bdc4dbf9ab8b1db464aa254
sarinosas <sarinosas12@gmail.com>
2025-03-04 17:48:36 +0200

    added stoplight feature to obstacle script
    obstacle cycles between green and red colour

--- Assets/Scripts/Stoplight.cs
+++ Assets/Scripts/Stoplight.cs
@@ -7,22 +7,34 @@ public class Obstacle : MonoBehaviour
     public SpriteRenderer spriteRenderer;
 
     [SerializeField]
-    float x_hitbox, y_hitbox;
+    float update_time;
     [SerializeField]
     Sprite sprite;
 
-    
 
+    
     // Start is called before the first frame update
     void Start()
     {
        
         spriteRenderer.sprite = sprite;
+        spriteRenderer.color = Color.red;
     }
 
+
+    float timePassed = 0f;
     // Update is called once per frame
     void Update()
     {
-        
+        timePassed += Time.deltaTime;
+        if(timePassed>update_time)
+        {
+            spriteRenderer.color = Color.green;
+        }
+        if(timePassed>update_time*2)
+        {
+            spriteRenderer.color = Color.red;
+            timePassed = 0f;
+        }
     }
 }
