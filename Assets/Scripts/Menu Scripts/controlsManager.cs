e3726acffdbbb94ef23beb232f7712881e511155
sarinosas <sarinosas12@gmail.com>
2025-03-31 18:47:29 +0300

    Added UNFINISHED settings menu with controls
    needs finishing

---
+++ Assets/Scripts/Menu Scripts/controlsManager.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using TMPro;
+
+public class controlsManager : MonoBehaviour
+{
+    static public Dictionary<string, KeyCode> controls = new Dictionary<string, KeyCode>();
+
+    public bool listening = false;
+
+    public string control_name="";
+
+    public static bool done = false;
+
+    public Button[] buttons;
+    void Awake()
+    {
+        if(!done)
+        {
+            controls.Add("forward", KeyCode.D);
+            controls.Add("backward", KeyCode.A);
+            controls.Add("upward", KeyCode.W);
+            controls.Add("downward", KeyCode.S);
+            controls.Add("jump", KeyCode.Space);
+            controls.Add("nitrous", KeyCode.Q);
+            controls.Add("headlight", KeyCode.L);
+            controls.Add("honk", KeyCode.H);
+            controls.Add("gear", KeyCode.P);
+            done = true;
+        }
+        
+
+    }
+
+    public void changeControls(string control)
+    {
+        listening = true;
+        control_name = control;
+        //print(controls[control]);
+        foreach(KeyCode key in System.Enum.GetValues(typeof(KeyCode)))
+        {
+            
+            if (Input.GetKeyDown(key))
+            {
+                controls[control] = key;
+                print("changed "+control+" to "+key);
+                listening = false;
+                foreach(Button button in buttons)
+                {
+                    
+                    if(button.name == control+"Button")
+                    {
+                        //button.GetComponent<TextMeshProUGUI>()[0].text = key.ToString();
+                        button.GetComponentsInChildren<TextMeshProUGUI>()[0].text = key.ToString();
+                        
+                    }
+                }
+                break;
+            }
+        }
+    }
+    void Update()
+    {
+        if(listening)
+        {
+            changeControls(control_name);
+        }
+    }
+}
