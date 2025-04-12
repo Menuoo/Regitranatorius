7245b252f9476e6fc512f10fc47d45866158f6ec
Menuoo <lietuvis7080@gmail.com>
2025-04-12 22:26:40 +0300

    Added routing + ...
    -Added NPC car route system (functional but could definitely be improved)
    -Optimised Road: three lanes, the dashed line is overlayed with the solid, but it's only active if the solid is active

--- Assets/Scripts/NPC_Car.cs
+++ Assets/Scripts/NPC_Car.cs
@@ -1,7 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+using UnityEngine.UIElements;
 
 public class NPC_Car : MonoBehaviour
 {
@@ -9,29 +12,66 @@ public class NPC_Car : MonoBehaviour
     GameObject wheel1, wheel2;
 
     [SerializeField]
-    float speed = 20f;
-
-    [SerializeField]
-    float distance = 40f;
+    float speed = 1f;
 
     float3 startPos;
+    int pathLength, current;
+
+    [SerializeField]
+    GameObject[] path;
+
+    Vector3[] paths;
+
+    Vector3 carPos;
 
     // Start is called before the first frame update
     void Start()
     {
-        startPos = transform.localPosition;
+        startPos = transform.position;
+        pathLength = path.Length;
+
+        current = 0;
+
+
+        paths = new Vector3 [pathLength];
+        int i = 0;
+        foreach (GameObject obj in path)
+        {
+            paths[i] = obj.transform.position;
+
+            i++;
+        }
+
+        carPos = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(new Vector2(speed * Time.deltaTime, 0f)); // actually makes the car move
         wheel1.transform.Rotate(new Vector3(0f, 0f, -speed / 2f));  // rotates wheel1
         wheel2.transform.Rotate(new Vector3(0f, 0f, -speed / 2f));  // rotates wheel2
 
-        if (transform.localPosition.x - startPos.x > distance)
+        CarMove();
+    }
+
+    private void CarMove()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, paths[current], speed * Time.deltaTime);
+
+        if ( math.abs(transform.position.x - paths[current].x) < 0.01f)
         {
-            transform.localPosition = startPos;
+            NextPath();
         }
     }
+
+    private void NextPath()
+    {
+        current++;
+        if (current == pathLength)
+        {
+            current = 0;
+            transform.position = startPos;
+        }
+        carPos = transform.position;
+    }    
 }
